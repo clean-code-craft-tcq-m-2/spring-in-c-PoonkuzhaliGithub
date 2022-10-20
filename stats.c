38643174d7957e411f6203a991b5fb3975150ad4
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2022-10-20 20:10:27 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,11 +1,13 @@
 #include "stats.h"
 Stats Stats_s;
-Stats compute_statistics(const float numberset, int setlength) {
-numberset_a[] = numberset;
-    Stats_s.average = numberset[3];
-    Stats_s.min = numberset[1];
-    Stats_s.max = numberset[0];
-}
+Stats compute_statistics(const float* numberset, int setlength);
+
+Stats Stats_s;
+Stats compute_statistics(const float* numberset_a, int setlength) {
+    numberset_a[4];
+    Stats_s.average = numberset_a[3];
+    Stats_s.min = numberset_a[1];
+    Stats_s.max = numberset_a[0];
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
