7e1ba572333ec80afba3bc4096fe4ded2c057482
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2022-10-20 07:18:10 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -5,8 +5,8 @@ typedef struct
   float min;
   float max;
 } Stats;
-  
-struct Stats compute_statistics(const float numberset, int setlength);
+ 
+Stats compute_statistics(const float numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
